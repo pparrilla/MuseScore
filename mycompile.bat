bc16de6454bede20559c5be3a4b818944aa1a67f
Tommaso Cucinotta <tommaso.cucinotta@gmail.com>
2019-07-07 14:52:06 +0200

    mycompile.bat: update to Qt 5.12.3

--- mycompile.bat
+++ mycompile.bat
@@ -1,6 +1,12 @@
-PATH=C:\Qt\Qt5.11.2\5.11.2\mingw53_32\bin;C:\Qt\Qt5.11.2\Tools\mingw530_32\bin;C:\MinGW\bin;C:\Program Files\CMake\bin;%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\;C:\msys32\usr\bin;C:\Program Files (x86)\WiX Toolset v3.11\bin;C:\Program Files\Git\cmd
+cls
+echo %time%
 
-#mingw32-make -f Makefile.mingw revision
+PATH=C:\Qt\5.12.3\mingw73_32\bin;C:\Qt\Tools\mingw730_32\bin
+PATH=%PATH%;C:\Program Files\CMake\bin;C:\Program Files\Git\cmd
+PATH=%PATH%;%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\;C:\msys32\usr\bin
+PATH=%PATH%;C:\Program Files (x86)\WiX Toolset v3.11\bin
+
+mingw32-make -f Makefile.mingw revision
 mingw32-make -f Makefile.mingw release BUILD_PCH=ON
 mingw32-make -f Makefile.mingw installrelease
 mingw32-make -f Makefile.mingw package
